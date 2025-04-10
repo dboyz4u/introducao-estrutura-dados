67c856f790a20a2e6e7a655b31e64ce6cf3895d7
dboyz4u <gustavo.pachecooo.plus@gmail.com>
2025-04-10 00:43:42 +0000

    introducao array

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 let allison_nota_av1 = 8;
 let allison_nota_av2 = 9.6;
-let allison_media = (allison_nota_av1 allison_nota_av2) /2
+let allison_media = (allison_nota_av1 + allison_nota_av2) /2
 
-console.log("média Allison: " + allison_media):
+console.log("média Allison: " + allison_media);
+
+console.log("exemplo com arrays")
+let notas_allison = [8,9,6, ];
+console.log(notas_allison);
+
+console.log("AV1: "+notas_allison[0]);
+console.log("AV2: "+notas_allison[1]);
+notas_allison[2] = (notas_allison[0] + notas_allison[1] ) /2;
+console.log("média: "+notas_allison[2]);
+console.log("todas: "+notas_allison);
